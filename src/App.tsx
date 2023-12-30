ef50947e0dc348dfef81e3fc60e1c6628681fb41
dan-j-x <dansaddress@proton.me>
2023-12-30 13:12:25 +1100

    Remove unused react-panel code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import type { API } from "../types/electron";
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import Logger from "./Logger";
-import Counsel from "./Counsel";
 
 declare global {
   interface Window {
@@ -12,17 +10,6 @@ declare global {
 
 function App() {
   return <Logger />;
-  {
-    /* <PanelGroup autoSaveId="example" direction="horizontal">
-      <Panel id="logger" order={1} defaultSizePercentage={100}>
-        <Logger />
-      </Panel>
-       <PanelResizeHandle style={{ width: "5px", backgroundColor: "grey" }} />
-      <Panel id="council" order={2} defaultSizePercentage={50}>
-        <Counsel />
-      </Panel> 
-    </PanelGroup> */
-  }
 }
 
 export default App;
