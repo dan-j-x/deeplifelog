273e15023ba16cfde4b63a7f4f1644a0afd85f85
dan-j-x <dansaddress@proton.me>
2023-12-30 13:02:14 +1100

    Fix capitalization in sql inserts

--- dist-data/initial_schema.sql
+++ dist-data/initial_schema.sql
@@ -36,15 +36,20 @@ create table Activity (
     foreign key (kind) references ActivityKind(code) on delete cascade
 );
 
-insert into MoodKind values ('1F600', 'happy',false);
-insert into MoodKind values ('1F641', 'sad',false);
-insert into MoodKind values ('1F62E', 'surprised',false);
-insert into MoodKind values ('1F629', 'weary',false);
-insert into MoodKind values ('1F971', 'tired',false);
-insert into MoodKind values ('1F914', 'thinking',false);
-insert into ActivityKind values ('1F3AE', 'gaming',false);
-insert into ActivityKind values ('1F3CB', 'lifting',false);
-insert into ActivityKind values ('1F4D6', 'reading',false);
+insert into MoodKind values ('1f600', 'happy', false);
+insert into MoodKind values ('1f641', 'sad', false);
+insert into MoodKind values ('1f62e', 'surprised', false);
+insert into MoodKind values ('1f629', 'weary', false);
+insert into MoodKind values ('1f971', 'tired', false);
+insert into MoodKind values ('1f914', 'thinking', false);
+insert into MoodKind values ('1f92f', 'mind blown', false);
+insert into ActivityKind values ('1f3ae', 'gaming', false);
+insert into ActivityKind values ('1f3cb', 'lifting', false);
+insert into ActivityKind values ('1f4d6', 'reading', false);
+insert into ActivityKind values ('1f6cf-fe0f', 'going to bed', false);
+insert into ActivityKind values ('1f6b6', 'walking', false);
+insert into ActivityKind values ('2615', 'coffee', false);
+insert into ActivityKind values ('1f3b6', 'listening to music', false);
 
 create table Day (
     date        text primary key,
