ca4ae30d4de61d59ca13f370ca0590c39c24a9a1
dan-j-x <dansaddress@proton.me>
2024-01-06 12:37:58 +1100

    Get app icon to load

--- electron/main.ts
+++ electron/main.ts
@@ -218,7 +218,7 @@ function openChannels() {
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "icon.png"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
